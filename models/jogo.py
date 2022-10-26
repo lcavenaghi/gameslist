dffd0b3ae5f8616fd35bf163e1ce4623dbbaf26f
Lucas Cavenaghi <lucas.ag.cv@gmail.com>
2022-10-26 18:29:45 -0300

    atualiza jogos

--- models/jogo.py
+++ models/jogo.py
@@ -7,4 +7,8 @@ jogo = api.model('jogo', {
     'dataLancamento': fields.Date(required=True, description='Data de lançamento do jogo'),
     'descricao': fields.String(required=True, description='Descrição do jogo'),
     'link': fields.String(required=True, description='Link do jogo'),
+    'plataforma': fields.String(required=True, description='Id da Plataforma do jogo'),
+    'desenvolvedora': fields.String(required=True, description='Id da desenvolvedora do jogo'),
+    'loja': fields.String(required=True, description='Id da loja do jogo'),
+    'tags': fields.List(fields.String(required=True, description='Ids de tags do jogo'))
 }, strict=True)
--- models/jogo_update.py
+++ models/jogo_update.py
@@ -7,4 +7,8 @@ jogo_update = api.model('jogo_update', {
     'dataLancamento': fields.Date(required=False, description='Data de lançamento do jogo'),
     'descricao': fields.String(required=False, description='Descrição do jogo'),
     'link': fields.String(required=False, description='Link do jogo'),
+    'plataforma': fields.String(required=True, description='Id da Plataforma do jogo'),
+    'desenvolvedora': fields.String(required=True, description='Id da desenvolvedora do jogo'),
+    'loja': fields.String(required=True, description='Id da loja do jogo'),
+    'tags': fields.List(fields.String(required=True, description='Ids de tags do jogo'))
 }, strict=True)
